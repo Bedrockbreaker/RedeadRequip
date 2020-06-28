c83a8625b55bf5c3ff2eb5a00551e0135dd90c90
Bedrockbreaker <jeb_searle@yahoo.com>
2020-06-28 14:28:11 -0700

    v1.2 update

--- RRItem.cs
+++ RRItem.cs
@@ -15,21 +15,21 @@ namespace RedeadRequip {
 			Item[] preDeathMiscDyes = person.preDeathMiscDyes;
 			if (preDeathArmor != null) {
 				if (item.headSlot + item.bodySlot + item.legSlot > 0) {
-					if (config.allowArmorEquip) if (checkItem(item, true, player, preDeathArmor, player.armor, config, 3)) return true; // Non vanity armor slots are 0-2
-					if (config.allowVanityArmorEquip) if (checkItem(item, true, player, preDeathArmor, player.armor, config, 13, 10)) return true; // vanity armor slots are 10-12
+					if (config.allowArmorEquip && checkItem(item, true, player, preDeathArmor, player.armor, config, 3)) return true; // Non vanity armor slots are 0-2
+					if (config.allowVanityArmorEquip && checkItem(item, true, player, preDeathArmor, player.armor, config, 13, 10)) return true; // vanity armor slots are 10-12
 				}
 				if (item.accessory) {
-					if (config.allowAccessoryEquip) if(checkItem(item, false, player, preDeathArmor, player.armor, config, 10, 3)) return true; // non vanity accessory slots are 3-9 (why are there 7?)
-					if (config.allowVanityAccessoryEquip) if(checkItem(item, false, player, preDeathArmor, player.armor, config, 20, 13)) return true; // vanity accessory slots are 13-19 (why are there 7?)
+					if (config.allowAccessoryEquip && checkItem(item, false, player, preDeathArmor, player.armor, config, 10, 3)) return true; // non vanity accessory slots are 3-9
+					if (config.allowVanityAccessoryEquip && checkItem(item, false, player, preDeathArmor, player.armor, config, 20, 13)) return true; // vanity accessory slots are 13-19
 				}
 			}
 			if (item.dye > 0 && config.allowDyeEquip) {
-				if (preDeathDyes != null) if (checkItem(item, true, player, preDeathDyes, player.dye, config)) return true;
-				if (preDeathMiscDyes != null) if (checkItem(item, true, player, preDeathMiscDyes, player.miscDyes, config)) return true;
+				if (preDeathDyes != null && checkItem(item, true, player, preDeathDyes, player.dye, config)) return true;
+				if (preDeathMiscDyes != null && checkItem(item, true, player, preDeathMiscDyes, player.miscDyes, config)) return true;
 			}
-			if ((item.ammo > 0 && !item.notAmmo) && config.allowAmmoEquip && preDeathInv != null) if(checkItem(item, false, player, preDeathInv, player.inventory, config, 58, 54)) return true; // Ammo slots are 54-57
-			if ((item.mountType > 0 || item.shoot > 0) && config.allowMiscEquip && preDeathMiscEquips != null) if(checkItem(item, true, player, preDeathMiscEquips, player.miscEquips, config)) return true;
-			if ((config.allowInventoryEquip || config.allowHotbarEquip) && preDeathInv != null) if(checkItem(item, true, player, preDeathInv, player.inventory, config, config.allowInventoryEquip ? player.inventory.Length : 10)) return true;
+			if (item.ammo > 0 && !item.notAmmo && config.allowAmmoEquip && preDeathInv != null && checkItem(item, false, player, preDeathInv, player.inventory, config, 58, 54)) return true; // Ammo slots are 54-57
+			if ((item.mountType > 0 || item.shoot > 0) && config.allowMiscEquip && preDeathMiscEquips != null && checkItem(item, true, player, preDeathMiscEquips, player.miscEquips, config)) return true;
+			if ((config.allowInventoryEquip || config.allowHotbarEquip) && preDeathInv != null && checkItem(item, true, player, preDeathInv, player.inventory, config, config.allowInventoryEquip ? player.inventory.Length : 10)) return true;
 			return true;
 		}
 
@@ -38,8 +38,8 @@ namespace RedeadRequip {
 			for (int i = start; i < end; i++) {
 				if (!itemIn.IsTheSameAs(preDeathInv[i])) continue;
 				preDeathInv[i].TurnToAir();
-				if (!currentInv[i].IsAir && !config.allowItemReplacement) return false;
 				if (!currentInv[i].IsAir) {
+					if (!config.allowItemReplacement) return false;
 					int firstEmpty = -1;
 					bool isMainInvSlot = false;
 					if (!checkMainInvFirst) firstEmpty = Array.FindIndex(currentInv.Skip(start-1).Take(end - start).ToArray(), slot => slot.IsAir);
@@ -47,9 +47,7 @@ namespace RedeadRequip {
 						// 0-49 are main slots. The last 9 slots are the coin, ammo, and trash slots. We don't want to put replaced items there.
 						firstEmpty = Array.FindIndex(player.hbLocked ? player.inventory.Skip(10).Take(40).ToArray() : player.inventory.Take(50).ToArray(), slot => slot.IsAir) + (player.hbLocked ? 10 : 0);
 						isMainInvSlot = true;
-						mod.Logger.Info(firstEmpty);
 					}
-					mod.Logger.Info(firstEmpty);
 					if (firstEmpty == -1 && !config.allowItemDrop) return false;
 					if (firstEmpty > -1) {
 						if (isMainInvSlot) {
@@ -61,7 +59,7 @@ namespace RedeadRequip {
 						Item.NewItem(player.getRect(), currentInv[i].type);
 					}
 				}
-				if (itemIn.dye == 0 || itemIn.stack == 1) {
+				if (itemIn.dye == 0) {
 					currentInv[i] = itemIn.Clone();
 					itemIn.TurnToAir();
 				} else {
@@ -69,10 +67,11 @@ namespace RedeadRequip {
 					singleDye.stack = 1;
 					currentInv[i] = singleDye;
 					itemIn.stack--;
-					checkItem(itemIn, checkMainInvFirst, player, preDeathInv, currentInv, config, end, start);
+					if (itemIn.stack > 0) return checkItem(itemIn, checkMainInvFirst, player, preDeathInv, currentInv, config, end, start);
 				}
 				return true;
 			}
+			if (itemIn.dye > 0 && currentInv == player.dye) return checkItem(itemIn, checkMainInvFirst, player, player.GetModPlayer<RRPlayer>().preDeathMiscDyes, player.miscDyes, config);
 			return false;
 		}
 	}
--- RRPlayer.cs
+++ RRPlayer.cs
@@ -12,11 +12,51 @@ namespace RedeadRequip {
 		public Item[] preDeathMiscDyes;
 		public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource) {
 			RRPlayer deadman = player.GetModPlayer<RRPlayer>();
-			deadman.preDeathInv = (Item[]) player.inventory.Clone();
-			deadman.preDeathArmor = (Item[]) player.armor.Clone();
-			deadman.preDeathMiscEquips = (Item[]) player.miscEquips.Clone();
-			deadman.preDeathDyes = (Item[]) player.dye.Clone();
-			deadman.preDeathMiscDyes = (Item[]) player.miscDyes.Clone();
+			Item[] invClone = (Item[])player.inventory.Clone();
+			if (deadman.preDeathInv != null) {
+				for (int i = 0; i < invClone.Length; i++) {
+					if (!invClone[i].IsAir) deadman.preDeathInv[i] = invClone[i];
+				}
+			} else {
+				deadman.preDeathInv = invClone;
+			}
+
+			Item[] armorClone = (Item[])player.armor.Clone();
+			if (deadman.preDeathArmor != null) {
+				for (int i = 0; i < armorClone.Length; i++) {
+					if (!armorClone[i].IsAir) deadman.preDeathArmor[i] = armorClone[i];
+				}
+			} else {
+				deadman.preDeathArmor = armorClone;
+			}
+
+			Item[] miscEquipsClone = (Item[])player.miscEquips.Clone();
+			if (deadman.preDeathMiscEquips != null) {
+				for (int i = 0; i < miscEquipsClone.Length; i++) {
+					if (!miscEquipsClone[i].IsAir) deadman.preDeathMiscEquips[i] = miscEquipsClone[i];
+				}
+			} else {
+				deadman.preDeathMiscEquips = miscEquipsClone;
+			}
+
+			Item[] dyesClone = (Item[])player.dye.Clone();
+			if (deadman.preDeathDyes != null) {
+				for (int i = 0; i < dyesClone.Length; i++) {
+					if (!dyesClone[i].IsAir) deadman.preDeathDyes[i] = dyesClone[i];
+				}
+			} else {
+				deadman.preDeathDyes = dyesClone;
+			}
+
+			Item[] miscDyesClone = (Item[])player.miscDyes.Clone();
+			if (deadman.preDeathMiscDyes != null) {
+				for (int i = 0; i < miscDyesClone.Length; i++) {
+					if (!miscDyesClone[i].IsAir) deadman.preDeathMiscDyes[i] = miscDyesClone[i];
+				}
+			} else {
+				deadman.preDeathMiscDyes = miscDyesClone;
+			}
+
 			return true;
 		}
 	}
